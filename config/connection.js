aaa9a66188fc2061844b4c66c699c67fe593cbed
justinhwbhang <justinhwbhang@gmail.com>
2023-02-11 22:48:01 -0800

    changed to local host

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'businessdb',
   port: 3306
 });
 
